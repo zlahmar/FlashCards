954897150eb99d36db680dc7ae422786bc770c4d
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2023-12-08 09:30:11 +0100

    init firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,31 @@ import { BrowserRouter } from "react-router-dom";
 import { SignIn } from "./components/SignIn/SignIn";
 import { SignUp } from "./components/SignUp/SignUp";
 import { LayoutConnexion } from "./layout/LayoutConnexion";
+import { useState } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { authentification } from "./services/firebase";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  onAuthStateChanged(authentification, (user) => {
+    setCurrentUser(user);
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<LayoutConnexion />}>
-          <Route path="/connexion" element={<SignIn />} />
-          <Route path="/inscription" element={<SignUp />} />
-        </Route>
-        <Route path="*" element={<h1> Erreur 404</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={currentUser}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<LayoutConnexion />}>
+            <Route path="/connexion" element={<SignIn />} />
+            <Route path="/inscription" element={<SignUp />} />
+          </Route>
+          <Route path="*" element={<h1> Erreur 404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import { signOut } from "firebase/auth";
+import { authentification } from "@/services/firebase";
+import { Button } from "@/components/ui/button";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const currentUser = useContext(AuthContext)
+  
+  return (
+    <div>
+      <h1>HOME</h1>
+      {currentUser ? `Je suis connecté en tant que ${currentUser.email}` : "Je ne suis pas connecté"}
+      
+      <Button onClick={() => signOut(authentification)}>Logout</Button>
+    </div>
+  );
 };
